2202b5b698a573641a9d5cb8f5ee1afa329dfd00
m.naghsh <m.naghsh@CLN18598>
2022-05-10 18:13:09 +0430

    some changes.

--- src/app/checkList/evaluationDiscrepanciesReport/evaluationDiscrepanciesReport.component.ts
+++ src/app/checkList/evaluationDiscrepanciesReport/evaluationDiscrepanciesReport.component.ts
@@ -1,11 +1,11 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, DebugElement, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import * as moment from 'jalali-moment'
 import { CommonService } from 'src/app/services/common.service';
-import { evaluationDiscrepanciesReportService } from 'src/app/services/evaluationDiscrepanciesReport/evaluationDiscrepanciesReport.service';
+import { evaluationDiscrepanciesReportsService } from 'src/app/services/evaluationDiscrepanciesReports/evaluationDiscrepanciesReports.service';
 import { RequestChecklistService } from 'src/app/services/requestChecklistService/RequestChecklistService';
 import { SchedulingService } from 'src/app/services/scheduling/scheduling.service';
 import { UsersComponent } from 'src/app/users/users.component';
@@ -26,7 +26,7 @@ export class EvaluationDiscrepanciesReportComponent implements OnInit {
     { value: 4, viewValue: 'روزانه' }
   ]
 
-  displayedColumns = ['number', 'namAssessorHsrch', 'assessmentDid', 'numberOfDuties', 'namPeriodHsrch'];
+  displayedColumns = ['number', 'NAM_ASSESSOR_HSRCH', 'PreviouslyEvaluated', 'NumberOfDuties', 'NAM_PERIOD_HSRCH'];
   listOfAllSchedulings: any;
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
@@ -40,8 +40,14 @@ export class EvaluationDiscrepanciesReportComponent implements OnInit {
   schedules: any;
   period: { value: number; viewValue: string; }[];
   selectedPeriod;
+  dateMinusYear: moment.Moment;
+  dateMinusMonth: moment.Moment;
+  dateMinusWeek: Date;
+
+
+
   constructor(public commonService: CommonService,
-    public evaluationDiscrepancies: evaluationDiscrepanciesReportService,
+    public evaluationDiscrepancies: evaluationDiscrepanciesReportsService,
     private dialog: MatDialog,
     public requestChecklist: RequestChecklistService,
     public SchedulingService: SchedulingService,
@@ -50,151 +56,229 @@ export class EvaluationDiscrepanciesReportComponent implements OnInit {
 
   ngOnInit(): void {
     this.newRowObj = {}
-    let sumNumNumberHsrch
-    this.getAllSchedules();
+
     this.period = [
       { value: 1, viewValue: 'هفتگی' },
-      { value: 2, viewValue: 'ماهانه'},
+      { value: 2, viewValue: 'ماهانه' },
       { value: 3, viewValue: 'سالانه' },
       { value: 4, viewValue: 'تاریخ انتخابی' },
     ];
 
+
   }
+  getAllSchedules() {
 
-  public getAllSchedules() {
-    console.log('getAllSchedules')
+    if (this.selectedPeriod != 4) {
+      this.commonService.loading = true;
+      this.evaluationDiscrepancies.selectEvaluationDiscrepanciesReports(this.selectedPeriod).subscribe(
+        // this.evaluationDiscrepancies.selectEvaluationDiscrepanciesReports(moment(time.toString()), this.selectedPeriod).subscribe(
 
-    let assessmentDid = []
-    let filteredRequests = []
-    let obj = []
-    this.commonService.loading = true;
-    this.requestChecklist.selectAllListOfRequestCheckLists().subscribe((success) => {
-      this.requests = success;
-      this.evaluationDiscrepancies.selectAllListOfevaluationDiscrepancies().subscribe((success) => {
-        this.schedules = success
-        this.schedules.forEach(item1 => {
-
-          this.requests.forEach(item2 => {
-
-            let requestDateHsrch = moment(item2.requestDateHsrch, 'YYYY/MM/DD');
-            let dateMinusWeek = moment(new Date(Date.now() - 7 * 24 * 60 * 60 * 1000), 'YYYY/MM/DD');
-            let dateMinusMonth = moment(new Date(Date.now() - 30 * 24 * 60 * 60 * 1000), 'YYYY/MM/DD');
-            let dateMinusYear = moment(new Date(Date.now() - 365 * 24 * 60 * 60 * 1000), 'YYYY/MM/DD');
-
-            switch (this.selectedPeriod) {
-              case 1:
-                if (requestDateHsrch > dateMinusWeek) {
-                  filteredRequests.push(item2)
-                  this.requests = filteredRequests
-                  if (item1.namAssessorHsrch == item2.namAssessorHsrch) {
-                    this.requests = this.requests.filter((value, index, self) =>
-                      index === self.findIndex((t) => (
-                        t.eRequestId === value.eRequestId
-                      ))
-                    )
-
-                    assessmentDid = [...assessmentDid, ...this.requests];
-
-                    assessmentDid = assessmentDid.filter((value, index, self) =>
-                    index === self.findIndex((t) => (
-                      t.eRequestId === value.eRequestId
-                    ))
-                  )
-                  }
-
-                }
-
-                break;
-              case 2:
-                if (requestDateHsrch > dateMinusMonth) {
-                  filteredRequests.push(item2)
-                  this.requests = filteredRequests
-                  if (item1.namAssessorHsrch == item2.namAssessorHsrch) {
-                    this.requests = this.requests.filter((value, index, self) =>
-                      index === self.findIndex((t) => (
-                        t.eRequestId === value.eRequestId
-                      ))
-                    )
-                    assessmentDid = [...assessmentDid, ...this.requests];
-
-                    
-                  }
-
-                }
-                break;
-              case 3:
-                if (requestDateHsrch > dateMinusYear) {
-                  filteredRequests.push(item2)
-                  this.requests = filteredRequests
-                  if (item1.namAssessorHsrch == item2.namAssessorHsrch) {
-                    this.requests = this.requests.filter((value, index, self) =>
-                      index === self.findIndex((t) => (
-                        t.eRequestId === value.eRequestId
-                      ))
-                    )
-                    assessmentDid.push(this.requests)
-                  }
-
-                }
-                break;
-              case 4:
-                let start = moment(this.startDate.nativeElement.value, 'jYYYY/jMM/jDD');
-                let end = moment(this.endDate.nativeElement.value, 'jYYYY/jMM/jDD');
-                if (requestDateHsrch >= start && requestDateHsrch <= end) {
-                  filteredRequests.push(item2)
-                  this.requests = filteredRequests
-                  if (item1.namAssessorHsrch == item2.namAssessorHsrch) {
-                    this.requests = this.requests.filter((value, index, self) =>
-                      index === self.findIndex((t) => (
-                        t.eRequestId === value.eRequestId
-                      ))
-                    )
-                    assessmentDid.push(this.requests)
-                  }
-
-                }
-                break;
-
-              default:
-
-
-                break;
-
-            }
-
-          });
-          assessmentDid = assessmentDid.filter((value, index, self) =>
-                    index === self.findIndex((t) => (
-                      t.eRequestId === value.eRequestId
-                    ))
-                  )
-          if (this.selectedPeriod == item1.numPeriodHsrch && this.selectedPeriod != 4) {
-            obj.push({
-              namAssessorHsrch: item1.namAssessorHsrch,
-              assessmentDid: assessmentDid.length,
-              numberOfDuties: item1.numberOfDuties,
-              namPeriodHsrch: item1.namPeriodHsrch
-
-            })
-          }
-          if (this.selectedPeriod == 4) {
-            obj.push({
-              namAssessorHsrch: item1.namAssessorHsrch,
-              assessmentDid: assessmentDid.length,
-              numberOfDuties: item1.numberOfDuties,
-              namPeriodHsrch: item1.namPeriodHsrch
-
-            })
-          }
-
-          this.dataSource = new MatTableDataSource(obj);
+        (success) => {
+          this.dataSource = new MatTableDataSource(success);
           this.dataSource.paginator = this.paginator;
           this.dataSource.sort = this.sort;
           this.commonService.loading = false;
-        });
-      })
-    })
+
+        })
+    }
+    else {
+
+debugger
+      this.commonService.loading = true;
+      let start = moment(this.startDate.nativeElement.value, 'jYYYY/jM/jD');
+      let startDate = start.locale('en').format('YYYY/M/D');
+      let end = moment(this.endDate.nativeElement.value, 'jYYYY/jM/jD');
+      let endDate = end.locale('en').format('YYYY/M/D');
+
+      this.evaluationDiscrepancies.selectEvaluationDiscrepanciesReportsByDate((this.startDate.nativeElement.value).toString(),(this.endDate.nativeElement.value).toString(), this.selectedPeriod).subscribe(
+        // this.evaluationDiscrepancies.selectEvaluationDiscrepanciesReports(moment(time.toString()), this.selectedPeriod).subscribe(
+
+        (success) => {
+
+          this.dataSource = new MatTableDataSource(success);
+          this.dataSource.paginator = this.paginator;
+          this.dataSource.sort = this.sort;
+          this.commonService.loading = false;
+
+        })
+    }
+
+    // this.dateMinusWeek=  new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+    //   this.dateMinusMonth = moment(new Date(Date.now() - 30 * 24 * 60 * 60 * 1000), 'YYYY/MM/DD');
+    // this.dateMinusYear = moment(new Date(Date.now() - 365 * 24 * 60 * 60 * 1000), 'YYYY/MM/DD');
+
+    //     switch (this.selectedPeriod) {
+    //       case 1:
+    //         this.evaluationDiscrepancies.selectEvaluationDiscrepanciesReports(Date.now(), this.selectedPeriod).subscribe(
+    //         // this.evaluationDiscrepancies.selectEvaluationDiscrepanciesReports(moment(time.toString()), this.selectedPeriod).subscribe(
+
+    //           (success) => { 
+
+
+    //           })
+    //         break;
+    //       case 2:
+    //         this.evaluationDiscrepancies.selectEvaluationDiscrepanciesReports(this.dateMinusWeek, this.selectedPeriod).subscribe(
+
+    //           (success) => { 
+    // console.log('ggggg',success)
+
+    //           })
+    //         break;
+    //       case 3:
+
+    //         break;
+    //       case 4:
+    //         let start = moment(this.startDate.nativeElement.value, 'jYYYY/jMM/jDD');
+    //         let end = moment(this.endDate.nativeElement.value, 'jYYYY/jMM/jDD');
+
+    //         break;
+
+    //       default:
+
+
+    //         break;
+
+    //     }
+
   }
+
+  // public oldGetAllSchedules() {
+  //   console.log('getAllSchedules')
+
+  //   let assessmentDid = []
+  //   let filteredRequests = []
+  //   let obj = []
+  //   this.commonService.loading = true;
+  //   this.requestChecklist.selectAllListOfRequestCheckLists().subscribe((success) => {
+  //     this.requests = success;
+  //     this.evaluationDiscrepancies.selectEvaluationDiscrepanciesReports('2022-04-02').subscribe((success) => {
+  //       this.schedules = success
+  //       this.schedules.forEach(item1 => {
+
+  //         this.requests.forEach(item2 => {
+
+  //           let requestDateHsrch = moment(item2.requestDateHsrch, 'YYYY/MM/DD');
+  //           let dateMinusWeek = moment(new Date(Date.now() - 7 * 24 * 60 * 60 * 1000), 'YYYY/MM/DD');
+  //           let dateMinusMonth = moment(new Date(Date.now() - 30 * 24 * 60 * 60 * 1000), 'YYYY/MM/DD');
+  //           let dateMinusYear = moment(new Date(Date.now() - 365 * 24 * 60 * 60 * 1000), 'YYYY/MM/DD');
+
+  //           switch (this.selectedPeriod) {
+  //             case 1:
+  //               if (requestDateHsrch > dateMinusWeek) {
+  //                 filteredRequests.push(item2)
+  //                 this.requests = filteredRequests
+  //                 if (item1.namAssessorHsrch == item2.namAssessorHsrch) {
+  //                   this.requests = this.requests.filter((value, index, self) =>
+  //                     index === self.findIndex((t) => (
+  //                       t.eRequestId === value.eRequestId
+  //                     ))
+  //                   )
+
+  //                   assessmentDid = [...assessmentDid, ...this.requests];
+
+  //                   assessmentDid = assessmentDid.filter((value, index, self) =>
+  //                   index === self.findIndex((t) => (
+  //                     t.eRequestId === value.eRequestId
+  //                   ))
+  //                 )
+  //                 }
+
+  //               }
+
+  //               break;
+  //             case 2:
+  //               if (requestDateHsrch > dateMinusMonth) {
+  //                 filteredRequests.push(item2)
+  //                 this.requests = filteredRequests
+  //                 if (item1.namAssessorHsrch == item2.namAssessorHsrch) {
+  //                   this.requests = this.requests.filter((value, index, self) =>
+  //                     index === self.findIndex((t) => (
+  //                       t.eRequestId === value.eRequestId
+  //                     ))
+  //                   )
+  //                   assessmentDid = [...assessmentDid, ...this.requests];
+
+
+  //                 }
+
+  //               }
+  //               break;
+  //             case 3:
+  //               if (requestDateHsrch > dateMinusYear) {
+  //                 filteredRequests.push(item2)
+  //                 this.requests = filteredRequests
+  //                 if (item1.namAssessorHsrch == item2.namAssessorHsrch) {
+  //                   this.requests = this.requests.filter((value, index, self) =>
+  //                     index === self.findIndex((t) => (
+  //                       t.eRequestId === value.eRequestId
+  //                     ))
+  //                   )
+  //                   assessmentDid.push(this.requests)
+  //                 }
+
+  //               }
+  //               break;
+  //             case 4:
+  //               let start = moment(this.startDate.nativeElement.value, 'jYYYY/jMM/jDD');
+  //               let end = moment(this.endDate.nativeElement.value, 'jYYYY/jMM/jDD');
+  //               if (requestDateHsrch >= start && requestDateHsrch <= end) {
+  //                 filteredRequests.push(item2)
+  //                 this.requests = filteredRequests
+  //                 if (item1.namAssessorHsrch == item2.namAssessorHsrch) {
+  //                   this.requests = this.requests.filter((value, index, self) =>
+  //                     index === self.findIndex((t) => (
+  //                       t.eRequestId === value.eRequestId
+  //                     ))
+  //                   )
+  //                   assessmentDid.push(this.requests)
+  //                 }
+
+  //               }
+  //               break;
+
+  //             default:
+
+
+  //               break;
+
+  //           }
+
+  //         });
+  //         assessmentDid = assessmentDid.filter((value, index, self) =>
+  //                   index === self.findIndex((t) => (
+  //                     t.eRequestId === value.eRequestId
+  //                   ))
+  //                 )
+  //         if (this.selectedPeriod == item1.numPeriodHsrch && this.selectedPeriod != 4) {
+  //           obj.push({
+  //             namAssessorHsrch: item1.namAssessorHsrch,
+  //             assessmentDid: assessmentDid.length,
+  //             numberOfDuties: item1.numberOfDuties,
+  //             namPeriodHsrch: item1.namPeriodHsrch
+
+  //           })
+  //         }
+  //         if (this.selectedPeriod == 4) {
+  //           obj.push({
+  //             namAssessorHsrch: item1.namAssessorHsrch,
+  //             assessmentDid: assessmentDid.length,
+  //             numberOfDuties: item1.numberOfDuties,
+  //             namPeriodHsrch: item1.namPeriodHsrch
+
+  //           })
+  //         }
+
+  //         this.dataSource = new MatTableDataSource(obj);
+  //         this.dataSource.paginator = this.paginator;
+  //         this.dataSource.sort = this.sort;
+  //         this.commonService.loading = false;
+  //       });
+  //     })
+  //   })
+  // }
+
+
 
 
   applyFilter(event: Event) {
---
+++ src/app/services/evaluationDiscrepanciesReports/evaluationDiscrepanciesReports.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ConfigService } from '../config.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class evaluationDiscrepanciesReportsService {
+
+  constructor(public configService: ConfigService) { }
+
+
+  // public selectAllEvaluationDiscrepanciesReports():Observable<any>{
+  //   return this.configService.get('evaluationDiscrepanciesReports');
+  // }
+  //this.userId ? this.userId : "",
+  public selectEvaluationDiscrepanciesReports(period): Observable<any> {
+    return this.configService.get('evaluationDiscrepanciesReports/' + Date.now()+','+ Date.now() + ',' + period);
+  }
+  public selectEvaluationDiscrepanciesReportsByDate(fromDate,toDate,period): Observable<any> {
+    return this.configService.get('evaluationDiscrepanciesReports/' + fromDate.toString()+','+ toDate.toString()+ ',' + period);
+  }
+
+  // public insertEvaluationDiscrepanciesReports(body):Observable<any>{
+  //   return this.configService.post('evaluationDiscrepanciesReports',body);
+  // }
+
+  // public updateEvaluationDiscrepanciesReports(id,body):Observable<any>{
+  //   return this.configService.put('evaluationDiscrepanciesReports/'+id,body);
+  // }
+  // public deleteEvaluationDiscrepanciesReports(id):Observable<any>{
+  //   return this.configService.delete('evaluationDiscrepanciesReports/'+id);
+  // }
+
+}
