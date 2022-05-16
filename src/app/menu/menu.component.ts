9ce655029aaab3b981ca8ec6ec0f5eed8f705ae8
m.naghsh <m.naghsh@CLN18598>
2022-05-16 10:04:15 +0430

    change right Menu.

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -13,6 +13,7 @@ export class MenuComponent implements OnInit {
   navBarItem: { label: string; path: string; icon: string; }[];
   rightMenu: { label: string; path: string; icon: string; }[];
   adminMenu: { label: string; path: string; icon: string; }[];
+  evaluationDiscrepanciesReport: { label: string; path: string; icon: string; }[];
 
 
   constructor(private auth: AuthenticationService,
@@ -37,28 +38,35 @@ export class MenuComponent implements OnInit {
       this.rightMenu = [
         { label: 'تعریف چک لیست', path: "/createCheckList", icon: "fa fa-pencil" },
         { label: 'انجام ارزیابی', path: "/checklistAssesment", icon: "fa fa-print" },
-        { label: 'چاپ گزارش', path: "/checklistReport", icon: "fa fa-newspaper-o" },
         { label: 'مدیریت مکان ها ', path: "/locations", icon: "fa fa-map-marker" },
         { label: 'مدیریت کاربران ', path: "/users", icon: "fa fa-users" },
-        { label: ' گزارش مغایرت ارزیابی های انجام شده', path: "/evaluationDiscrepanciesReportByAssessor", icon: "fa fa-newspaper-o" },
-        { label: ' گزارش مغایرت ارزیابی های انجام شده', path: "/evaluationDiscrepanciesReportByLocation", icon: "fa fa-newspaper-o" },
-        { label: ' گزارش مغایرت ارزیابی های انجام شده', path: "/evaluationDiscrepanciesReportByCheckList", icon: "fa fa-newspaper-o" },        { label: 'چاپ گزارش بر اساس نام ارزیاب و تاریخ', path: "/requestChecklistReport", icon: "fa fa-newspaper-o" },
         { label: 'خروج', path: "/login", icon: "fa fa-sign-out" },
       ]
+
+      this.evaluationDiscrepanciesReport = [
+        { label: 'چاپ گزارش چک لیست ها', path: "/checklistReport", icon: "fa fa-newspaper-o" },
+        { label: 'گزارش مغایرت ارزیابی های انجام شده بر اساس نام ارزیاب', path: "/evaluationDiscrepanciesReportByAssessor", icon: "fa fa-newspaper-o" },
+        { label: 'گزارش مغایرت ارزیابی های انجام شده بر اساس نام مکان', path: "/evaluationDiscrepanciesReportByLocation", icon: "fa fa-newspaper-o" },
+        { label: 'گزارش مغایرت ارزیابی های انجام شده بر اساس نام چک لیست', path: "/evaluationDiscrepanciesReportByCheckList", icon: "fa fa-newspaper-o" }, { label: 'چاپ گزارش بر اساس نام ارزیاب و تاریخ', path: "/requestChecklistReport", icon: "fa fa-newspaper-o" },
+
+      ];
 
     }
     else {
       this.rightMenu = [
         { label: 'تعریف چک لیست', path: "/createCheckList", icon: "fa fa-pencil" },
         { label: 'انجام ارزیابی', path: "/checklistAssesment", icon: "fa fa-print" },
-        { label: 'چاپ گزارش', path: "/checklistReport", icon: "fa fa-newspaper-o" },
         { label: 'مدیریت مکان ها ', path: "/locations", icon: "fa fa-map-marker" },
-        { label: ' گزارش مغایرت ارزیابی های انجام شده', path: "/evaluationDiscrepanciesReportByAssessor", icon: "fa fa-newspaper-o" },
-        { label: ' گزارش مغایرت ارزیابی های انجام شده', path: "/evaluationDiscrepanciesReportByLocation", icon: "fa fa-newspaper-o" },
-        { label: ' گزارش مغایرت ارزیابی های انجام شده', path: "/evaluationDiscrepanciesReportByCheckList", icon: "fa fa-newspaper-o" },
-        { label: 'چاپ گزارش بر اساس نام ارزیاب و تاریخ', path: "/requestChecklistReport", icon: "fa fa-newspaper-o" },
         { label: 'خروج', path: "/login", icon: "fa fa-sign-out" },
       ]
+      // this.evaluationDiscrepanciesReport = [
+      //   { label: 'چاپ گزارش چک لیست ها', path: "/checklistReport", icon: "fa fa-newspaper-o" },
+      //   { label: 'چاپ گزارش بر اساس نام ارزیاب و تاریخ', path: "/requestChecklistReport", icon: "fa fa-newspaper-o" },
+      //   { label: 'گزارش مغایرت ارزیابی های انجام شده بر اسا نام ارزیاب', path: "/evaluationDiscrepanciesReportByAssessor", icon: "fa fa-newspaper-o" },
+      //   { label: 'گزارش مغایرت ارزیابی های انجام شده بر اساس نام مکان', path: "/evaluationDiscrepanciesReportByLocation", icon: "fa fa-newspaper-o" },
+      //   { label: 'گزارش مغایرت ارزیابی های انجام شده بر اساس نام چک لیست', path: "/evaluationDiscrepanciesReportByCheckList", icon: "fa fa-newspaper-o" }, { label: 'چاپ گزارش بر اساس نام ارزیاب و تاریخ', path: "/requestChecklistReport", icon: "fa fa-newspaper-o" },
+
+      // ];
 
     }
 
